e7fb8d635bdb58c69610da3c62baba25f77364c6
Slepoyi <zub.dayu@mail.ru>
2023-03-15 13:38:21 +0300

    Renamed app context

---
+++ OrdersSystem.Data.Access/Context/ApplicationContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrdersSystem.Domain.Models.Actors;
+using OrdersSystem.Domain.Models.Ordering;
+using OrdersSystem.Domain.Models.Stock;
+
+namespace OrdersSystem.Data.Access.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<OrderPicker> OrderPickers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Sku> Skus { get; set; }
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+    }
+}
