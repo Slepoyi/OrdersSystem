ee0d05f661a902fb6da20d4bc147bcc729b1a50f
Slepoyi <zub.dayu@mail.ru>
2023-03-30 11:40:48 +0300

    Added order validator interface

---
+++ OrdersSystem.Data.Process/Validation/IOrderValidator.cs
@@ -0,0 +1,10 @@
+﻿using OrdersSystem.Domain.Models.Ordering;
+using OrdersSystem.Domain.Models.Stock;
+
+namespace OrdersSystem.Data.Process.Validation
+{
+    internal interface IOrderValidator
+    {
+        ValidationResult ValidateOrder(IEnumerable<OrderItem> order, IEnumerable<StockItem> stock);
+    }
+}
