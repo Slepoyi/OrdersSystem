83b4fc1dcbce50f4e8b3b3d0aaa1fd2fa135debf
Slepoyi <zub.dayu@mail.ru>
2023-03-30 11:43:02 +0300

    Added some logic to controllers

--- OrdersSystem.Api/Controllers/CustomerController.cs
+++ OrdersSystem.Api/Controllers/CustomerController.cs
@@ -1,43 +1,80 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using OrdersSystem.Data.Process.Validation;
-using OrdersSystem.Domain.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using OrdersSystem.Api.Auth.Middleware;
+using OrdersSystem.Data.Process.Services;
+using OrdersSystem.Domain.Enums;
+using OrdersSystem.Domain.Models.Auth;
+using OrdersSystem.Domain.Models.Ordering;
 
 namespace OrdersSystem.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Authorize(UserRole.Customer)]
+    [Route("api/customers/")]
     [ApiController]
     public class CustomerController : ControllerBase
     {
-        private readonly OrderValidator _orderValidator;
+        private readonly IOrderFlowManager _orderManager;
 
+        // create user order
+        // all skus are reserved in database
+        // return guid of order or error
         [HttpPost]
-        public async Task<IActionResult> CreateOrderAsync([FromBody] Dictionary<Sku, uint> orderSku)
+        public async Task<IActionResult> CreateOrderAsync([FromBody] IEnumerable<OrderItem> orderItems)
         {
+            if (HttpContext.Items["User"] is not User user)
+                return Problem("Error creating user identity.");
 
+            var stockItems = _orderManager.GetStockForOrderItems(orderItems);
 
-            if (!_orderValidator.ValidateOrder(orderSku))
-                return ValidationProblemsList; // 400 with custom message
+            var validationResult = _orderManager.ValidateOrder(orderItems, stockItems);
+            if (!validationResult.IsValid)
+                return BadRequest(validationResult.ErrorMessages);
 
-            var guid = _orderManager.CreateOrderAsync(orderSku);
-            return Ok(guid);
-            // create user order here
-            // all skus are reserved in database
-            // return guid of order or error
+            var order = await _orderManager.CreateOrderAsync(orderItems, user.Id, stockItems);
+            if (order is null)
+                return BadRequest();
+
+            return CreatedAtAction(nameof(GetByGuidAsync), new { id = order.Id }, order);
         }
 
+        // cancel order if possible
+        // rollback of reserve
         [HttpPost("{id}")]
         public async Task<IActionResult> CancelOrderAsync(Guid id)
         {
-            var cancelled = _orderManager.CancelOrderAsync(id);
+            if (HttpContext.Items["User"] is not User user)
+                return Problem("Error creating user identity.");
+
+            var order = await _orderManager.GetByGuidAsync(id);
+
+            if (order is null)
+                return NotFound();
+
+            if (order.Customer.Id != user.Id)
+                return Forbid("You cannot cancel this order.");
+
+            var cancelled = await _orderManager.CancelOrderAsync(order);
 
             if (!cancelled)
-                return SorryOrderCannotBeCancelledMessage; // 501 (or 409) with custom message
+                return Problem(detail: "Sorry, your order cannot be cancelled.", statusCode: 501);
 
             return Ok("Order was cancelled");
-            // cancel order if possible
-            // rollback of reserve
-            // should it be post?
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByGuidAsync(Guid id)
+        {
+            if (HttpContext.Items["User"] is not User user)
+                return Problem("Error creating user identity.");
+
+            var order = await _orderManager.GetByGuidAsync(id);
+
+            if (order is null)
+                return NotFound();
+
+            if (order.Customer.Id != user.Id)
+                return Forbid("You cannot track this order.");
+
+            return Ok(order);
         }
     }
 }
--- OrdersSystem.Api/Controllers/OrderPickerController.cs
+++ OrdersSystem.Api/Controllers/OrderPickerController.cs
@@ -1,37 +1,38 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using OrdersSystem.Domain.Models;
+using OrdersSystem.Domain.Models.Stock;
 
 namespace OrdersSystem.Api.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/pickers/")]
     [ApiController]
     public class OrderPickerController : ControllerBase
     {
-        [HttpPost("{id}")]
-        public async Task<IActionResult> CloseOrderAsync(Guid id, [FromBody] IEnumerable<Sku> orderSku)
-        {
-            var closed = _orderManager.CloseOrder(id, orderSku, User.Identity.Name);
-            if (!closed)
-                return SorryThereIsSomethingWrongWithRequestParams; // 400 with custom code
-            // close order, assign ordersku to order skus list and change status of an order
-            // find difference between final order and reserve and rollback stock of the difference
-        }
-        [HttpPost]
-        public async Task<IActionResult> GetNextOrderAsync()
-        {
-            //var order = _orderQueue.GetNextOrderAsync();
-            _orderManager.AssignOrder();
-            if (order is null)
-                return SorryNoOrderForYouRightNow(); // 404 with custom message
+        //[HttpPost("{id}")]
+        //public async Task<IActionResult> CloseOrderAsync(Guid id, [FromBody] IEnumerable<Sku> orderSku)
+        //{
+        //    var closed = _orderManager.CloseOrder(id, orderSku, User.Identity.Name);
+        //    if (!closed)
+        //        return SorryThereIsSomethingWrongWithRequestParams; // 400 with custom code
+        //    // close order, assign ordersku to order skus list and change status of an order
+        //    // find difference between final order and reserve and rollback stock of the difference
+        //}
+        //[HttpPost]
+        //public async Task<IActionResult> GetNextOrderAsync()
+        //{
+        //    //var order = _orderQueue.GetNextOrderAsync();
+        //    _orderManager.AssignOrder();
+        //    if (order is null)
+        //        return SorryNoOrderForYouRightNow(); // 404 with custom message
 
-            _orderManager.BeginOrderPickingAsync(order, User.Identity?.Name);
+        //    _orderManager.BeginOrderPickingAsync(order, User.Identity?.Name);
 
-            return Ok(order);
-            // get next order from order queue
-            // picker begins picking an order
-            // set order pickingstarted time, picker and status
-            // is it post or get really?
-        }
+        //    return Ok(order);
+        //    // get next order from order queue
+        //    // picker begins picking an order
+        //    // set order pickingstarted time, picker and status
+        //    // is it post or get really?
+        //}
     }
 }
