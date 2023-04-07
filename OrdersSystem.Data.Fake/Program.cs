b6036255ae1a326cc4bdf0a5a0f83a1945272763
Slepoyi <zub.dayu@mail.ru>
2023-04-07 20:57:20 +0300

    Added fake data generation proj

---
+++ OrdersSystem.Data.Fake/Program.cs
@@ -0,0 +1,11 @@
+﻿using Bogus;
+using OrdersSystem.Domain.Models.Ordering;
+
+Console.WriteLine("Hello, World!");
+
+var customerFaker = new Faker<Customer>()
+    .RuleFor(c => c.Id, Guid.NewGuid())
+    .RuleFor(c => c.Name, (f, c) => f.Name.FullName())
+    .RuleFor(c => c.Email, (f, c) => f.Internet.Email())
+    .RuleFor(c => c.Address, (f, c) => f.Address.StreetAddress(false))
+    .RuleFor(c => c.Phone, (f, c) => f.Phone.PhoneNumber("###-###-####")); // add orders
