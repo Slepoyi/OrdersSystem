b1094580b39be0c387f03922568ace2c3cbe22e5
Slepoyi <zub.dayu@mail.ru>
2023-05-08 11:26:57 +0300

    Added services

--- OrdersSystem.Api/Program.cs
+++ OrdersSystem.Api/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.EntityFrameworkCore;
 using OrdersSystem.Api.Auth.Middleware;
+using OrdersSystem.Api.Auth.Services;
 using OrdersSystem.Api.Options;
 using OrdersSystem.Data.Access.Context;
+using OrdersSystem.Data.Process.Services;
+using OrdersSystem.Data.Process.Validation;
+using OrdersSystem.Data.Refresh;
+using OrdersSystem.Domain.Helper;
+using OrdersSystem.Domain.Time;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,12 +18,20 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddHttpContextAccessor();
 builder.Services.AddDbContext<ApplicationContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("LaptopConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DesktopConnection"));
 });
 
 builder.Services.Configure<JwtOptions>(builder.Configuration.GetSection(JwtOptions.Section));
 
 builder.Services.AddHealthChecks().AddDbContextCheck<ApplicationContext>();
+
+builder.Services.AddTransient<IClock, Clock>();
+builder.Services.AddTransient<ICryptoProvider, Md5HashProvider>();
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddTransient<IJwtService, JwtService>();
+builder.Services.AddTransient<IOrderFlowManager, OrderFlowManager>();
+builder.Services.AddTransient<IOrderValidator, OrderValidator>();
+builder.Services.AddTransient<IRefreshDbSets, RefreshDbSets>();
 
 var app = builder.Build();
 
