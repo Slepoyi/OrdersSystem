1ce8394497ae9a684a9257994bd4b9acd843caa5
Slepoyi <zub.dayu@mail.ru>
2023-05-10 16:47:15 +0300

    Add faker options

---
+++ OrdersSystem.Data.Process/Options/FakerOptions.cs
@@ -0,0 +1,13 @@
+﻿namespace OrdersSystem.Data.Process.Options
+{
+    public class FakerOptions
+    {
+        public const string Section = "FakerOptions";
+
+        public int NumPickers { get; set; }
+        public int NumCustomers { get; set; }
+        public int NumSkus { get; set; }
+        public int NumOrderItems { get; set; }
+        public int NumOrders { get; set; }
+    }
+}
