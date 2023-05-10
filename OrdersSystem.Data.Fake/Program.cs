270fcb4aeb8a04a98e1c2ffd63487865df42ab5c
Slepoyi <zub.dayu@mail.ru>
2023-05-10 16:47:33 +0300

    Comment out unnecessary code

--- OrdersSystem.Data.Fake/Program.cs
+++ OrdersSystem.Data.Fake/Program.cs
@@ -1,19 +1,18 @@
-﻿using OrdersSystem.Data.Refresh;
-using System.Text.Json;
+﻿using System.Text.Json;
 
-DataGenerator.InitData();
+//DataGenerator.InitData();
 
-foreach (var u in DataGenerator.Users) Print(u);
-Console.WriteLine("_________________________________________________________________");
-foreach (var c in DataGenerator.Customers) Print(c);
-Console.WriteLine("_________________________________________________________________");
-foreach (var s in DataGenerator.Skus) Print(s);
-Console.WriteLine("_________________________________________________________________");
-foreach (var op in DataGenerator.OrderPickers) Print(op);
-Console.WriteLine("_________________________________________________________________");
-foreach (var o in DataGenerator.Orders) Print(o);
-Console.WriteLine("_________________________________________________________________");
-foreach (var si in DataGenerator.StockItems) Print(si);
+//foreach (var u in DataGenerator.Users) Print(u);
+//Console.WriteLine("_________________________________________________________________");
+//foreach (var c in DataGenerator.Customers) Print(c);
+//Console.WriteLine("_________________________________________________________________");
+//foreach (var s in DataGenerator.Skus) Print(s);
+//Console.WriteLine("_________________________________________________________________");
+//foreach (var op in DataGenerator.OrderPickers) Print(op);
+//Console.WriteLine("_________________________________________________________________");
+//foreach (var o in DataGenerator.Orders) Print(o);
+//Console.WriteLine("_________________________________________________________________");
+//foreach (var si in DataGenerator.StockItems) Print(si);
 
 void Print<T>(T entity)
 {
