5a3af967e1d26f5d3a3d02bfe68ecca5d6796fce
Slepoyi <zub.dayu@mail.ru>
2023-05-15 10:26:21 +0300

    Remove unused code

--- OrdersSystem.Domain/Models/Ordering/Order.cs
+++ OrdersSystem.Domain/Models/Ordering/Order.cs
@@ -19,41 +19,5 @@ namespace OrdersSystem.Domain.Models.Ordering
         public virtual OrderPicker OrderPicker { get; set; }
         public Guid OrderPickerId { get; set; }
         public OrderStatus OrderStatus { get; set; }
-
-        public decimal GetTotalPrice()
-        {
-            decimal totalPrice = 0;
-
-            foreach (var item in OrderItems)
-            {
-                totalPrice += item.Quantity * item.Sku.Price;
-            }
-
-            return totalPrice;
-        }
-
-        public bool ReduceSku(OrderItem item, uint quantity)
-        {
-            var currentItem = OrderItems.FirstOrDefault(s => s.Sku == item.Sku);
-            if (currentItem is null)
-                return false;
-
-            if (currentItem.Quantity <= quantity)
-                OrderItems.Remove(currentItem);
-            else
-                currentItem.ReduceQuantity(quantity);
-
-            return true;
-        }
-
-        public void AddSku(OrderItem item, uint quantity)
-        {
-            var currentItem = OrderItems.FirstOrDefault(s => s.Sku == item.Sku);
-
-            if (currentItem is null)
-                OrderItems.Add(item);
-            else
-                currentItem.IncreaseQuantity(quantity);
-        }
     }
 }
