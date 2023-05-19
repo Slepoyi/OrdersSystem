42850305d59955c8da0b1ba03f9d777da1585bfa
Slepoyi <zub.dayu@mail.ru>
2023-05-19 09:16:24 +0300

    Create OrderDto to keep private info private

---
+++ OrdersSystem.Domain/Models/Dto/OrderDto.cs
@@ -0,0 +1,14 @@
+﻿using OrdersSystem.Domain.Models.Ordering;
+
+namespace OrdersSystem.Domain.Models.Dto
+{
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+        public DateTime OpenTime { get; set; }
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+        public DateTime PickingStartTime { get; set; }
+        public DateTime CloseTime { get; set; }
+        public OrderStatus OrderStatus { get; set; }
+    }
+}
---
+++ OrdersSystem.Domain/Models/Extensions/ModelsExtensions.cs
@@ -0,0 +1,21 @@
+﻿using OrdersSystem.Domain.Models.Dto;
+using OrdersSystem.Domain.Models.Ordering;
+
+namespace OrdersSystem.Domain.Models.Extensions
+{
+    public static class ModelsExtensions
+    {
+        public static OrderDto ToOrderDto(this Order order)
+        {
+            return new OrderDto
+            {
+                Id = order.Id,
+                OrderItems = order.OrderItems,
+                OrderStatus = order.OrderStatus,
+                OpenTime = order.OpenTime,
+                CloseTime = order.CloseTime,
+                PickingStartTime = order.PickingStartTime
+            };
+        }
+    }
+}
