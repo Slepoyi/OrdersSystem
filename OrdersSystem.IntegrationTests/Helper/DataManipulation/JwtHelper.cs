0dd22b92863398bda04b1431a00725f550045532
Slepoyi <zub.dayu@mail.ru>
2023-05-31 23:15:36 +0300

    Simplify helper for jwt token query

--- OrdersSystem.IntegrationTests/Helper/DataManipulation/JwtHelper.cs
+++ OrdersSystem.IntegrationTests/Helper/DataManipulation/JwtHelper.cs
@@ -5,27 +5,13 @@ namespace OrdersSystem.IntegrationTests.Helper.DataManipulation
 {
     public class JwtHelper
     {
-        public static async Task<string> GetCustomerTokenAsync(HttpClient client)
-        {
-            var model = new LoginModel { Username = "Aliya_Cruickshank", Password = "PVBCbyoW" };
-
-            return await SendAuthRequestAsync(client, model);
-        }
-
-        public static async Task<string> GetPickerTokenAsync(HttpClient client)
-        {
-            var model = new LoginModel { Username = "Aliya_Cruickshank", Password = "PVBCbyoW" };
-
-            return await SendAuthRequestAsync(client, model);
-        }
-
-        private static async Task<string> SendAuthRequestAsync(HttpClient client, LoginModel model)
+        public static async Task<string?> GetTokenAsync(HttpClient client, LoginModel model)
         {
             var request = new HttpRequestMessage(HttpMethod.Post, "/api/auth/");
             request.Content = JsonContent.Create(model);
             var response = await client.SendAsync(request);
             var token = await response.Content.ReadFromJsonAsync<TokenModel>();
-            return token.Token;
+            return token?.Token;
         }
     }
 }
